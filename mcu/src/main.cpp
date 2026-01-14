c7cd4caaf1a460dd81543d1775063e536c10dd8e
Eren Balci <me@erenbalci.com>
2026-01-14 12:11:35 +0100

    refactor: streamline main loop and setup by integrating SamplingController and removing unused functions

--- mcu/src/main.cpp
+++ mcu/src/main.cpp
@@ -6,48 +6,19 @@
 #include "color_sampler.h"
 #include "color_sensor.h"
 #include "display.h"
+#include "sampling_controller.h"
 #include <Arduino.h>
 #include <Wire.h>
 
+// Hardware configuration
 Display display(128, 32, 21, 22);
 ColorSensor sensor(27, 14, 32, 33, 35, 26);
 ColorSampler sampler;
 Button button(13);
-// Bluetooth ble;  // BLE: uncomment to enable
+Bluetooth ble;
 
-void handleSampling(const RGBColor &color) {
-  sampler.addSample(color);
-
-  RGBColor avgColor = sampler.getAverage();
-  String avgColorName = sensor.detectColorName(avgColor);
-
-  display.showColorData(avgColor.red, avgColor.green, avgColor.blue,
-                        avgColorName);
-  sampler.printSample(color);
-}
-
-void handleSampleComplete() {
-  RGBColor avgColor = sampler.getAverage();
-  String avgColorName = sensor.detectColorName(avgColor);
-
-  sampler.printAverage(avgColor, avgColorName);
-  display.showColorData(avgColor.red, avgColor.green, avgColor.blue,
-                        avgColorName);
-
-  String bleData = String(avgColor.red) + "," + String(avgColor.green) + "," +
-                   String(avgColor.blue) + "," + avgColorName;
-  Serial.println("Average Color: " + bleData);
-  // ble.send(bleData);  // BLE: uncomment to enable
-
-  delay(3000);
-
-  sampler.reset();
-}
-
-void handleNormalMode(const RGBColor &color, const String &colorName) {
-  display.showColorData(color.red, color.green, color.blue, colorName);
-  sensor.printColorData(color, colorName);
-}
+// Controller
+SamplingController controller(display, sensor, sampler, button, ble);
 
 void setup() {
   Serial.begin(9600);
@@ -59,24 +30,21 @@ void setup() {
       ;
   }
 
+  display.showSplash();
+  delay(2500);
+
   display.showWelcome();
-  delay(2000);
+  delay(1500);
 
   sensor.begin();
   button.begin();
-  // ble.begin("Surface Color Detector");  // BLE: uncomment to enable
+  ble.begin("Surface Color Detector");
 
+  controller.begin();
   Serial.println("Setup complete!");
 }
 
 void loop() {
-  sensor.ensureLedOn(); // Ensure LED stays on
-  RGBColor color = sensor.readColor();
-
-  if (button.isPressed())
-    handleSampling(color);
-  else if (sampler.isSampling())
-    handleSampleComplete();
-
-  delay(100);
+  controller.update();
+  delay(30);
 }
