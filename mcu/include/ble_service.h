c6f1ce9e69d18e95f060974ccb4a00fbab92986c
Eren Balci <me@erenbalci.com>
2026-01-14 12:10:43 +0100

    refactor: reorganize includes and improve class formatting in ble_service.h

--- mcu/include/ble_service.h
+++ mcu/include/ble_service.h
@@ -2,16 +2,16 @@
 #define BLE_SERVICE_H
 
 #include <Arduino.h>
+#include <BLE2902.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
+#include <esp_bt.h>
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-class Bluetooth
-{
+class Bluetooth {
 private:
   BLEServer *pServer;
   BLECharacteristic *pCharacteristic;
